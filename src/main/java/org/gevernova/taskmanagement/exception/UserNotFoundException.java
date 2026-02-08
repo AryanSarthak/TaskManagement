1a292aaae9c6b4901e46eb3951865fcabf60ac38
XEROX112 <jayabhardwaz1@gmail.com>
2026-02-08 18:08:07 +0530

    Add UserNotFound Exception

---
+++ src/main/java/org/gevernova/taskmanagement/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.gevernova.taskmanagement.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String msg) {
+        super(msg);
+    }
+}
